9d70173caafd09a872f98b31570c5bf072c822f3
PraveenKumar263 <PraveenKumar263>
2024-08-04 15:36:54 +0800

    node http has been replaced with express basic setup

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-const { createServer } = require('http');
+// Import express module
+const express = require('express');
 
-const notes = [
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    }
-];
+// Create an express application
+const app = express();
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello World!\n');
-res.end(JSON.stringify(notes))
+// Port number
+const port = 3001;
+
+// Define a route handler for the default "GET" request "/"
+app.get('/', (request, response) => {
+  response.send('Hello World!');
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
-});
+// Start the server and list on port 3001
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+})
